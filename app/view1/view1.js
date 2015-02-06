dba86eed96ea4db0e2810f596ac337779730481c
Piotr B <b.piotr@gmail.com>
2015-02-06 20:43:07 +0100

    Angular first app

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,10 +5,49 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'ReadingListController'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('ReadingListController', [function() {
+  this.books = books;
+  this.genres = genres;
+  this.showForm = false;
+}])
+.directive('bookCover', function(){
+  return {
+    restrict: 'E',
+    templateUrl: 'view1/partials/book-cover.html',
+    replace: true
+  }
+})
+.directive('bookGenres', function(){
+  return {
+    restrict: 'E',
+    templateUrl: 'view1/partials/book-genres.html',
+    scope: {
+      genres: '='
+    }
+  }
+})
+.directive('reviewForm', function(){
+   return {
+     restrict: 'E',
+     templateUrl: 'view1/partials/review-form.html',
+     replace: true,
+     controller: function(){
+       this.book = {genres:{}};
 
-}]);
+       this.addReview = function(form){
+         books.push(this.book);
+         this.book = {genres:{}};
+         form.$setPristine();
+       }
+     },
+     controllerAs: 'reviewFormCtrl',
+     scope: {
+       books: '=',
+       genres: '='
+     }
+   }
+ });
